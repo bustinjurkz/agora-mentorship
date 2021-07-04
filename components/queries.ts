e3be16012806fa9753aa7ed0bfab5a52f015b0e6
Dustin Jurkaulionis <dustin.jurkaulionis@mlcgroup.co.nz>
2021-07-04 15:50:05 +1200

    mentee profile page and basic query

--- components/queries.ts
+++ components/queries.ts
@@ -15,3 +15,19 @@ gql`
     }
   }
 `;
+
+gql`
+  query GetMentees {
+    Mentee {
+      name
+      job_title_primary
+      job_title_secondary
+      bio
+      preferred_services
+      school
+      school_major
+      id
+      school_year
+    }
+  }
+`;
--- pages/mentee.tsx
+++ pages/mentee.tsx
@@ -1,14 +1,47 @@
+import PersonalInfo from 'components/PersonalInfo';
 import React from 'react';
-import styled from 'styled-components';
+import Loading from 'components/Loading';
+import ProfileDashboard from 'components/ProfileDashboard';
+import { BackgroundStyle } from 'components/helperFunctions';
+import MeetingsCalendar from 'components/meetings/MeetingsCalendar';
+import UpcomingMeetings from 'components/meetings/UpcomingMeetings';
+import PendingMeetings from 'components/meetings/PendingMeetings';
+import PastConnections from 'components/meetings/PastConnections';
+import { AdminStyle } from './mentor';
+import { useGetMenteesQuery } from 'generated/graphql';
+
 // import { useRouter } from 'next/router';
 
 const MenteeAdmin: React.FC = () => {
   // const router = useRouter();
-  return <MenteeAdminStyle>yeet</MenteeAdminStyle>;
+  const { data, loading } = useGetMenteesQuery();
+  if (loading) {
+    return <Loading />;
+  }
+  const renderNotificationBanner = () => {
+    return (
+      <BackgroundStyle backgroundColor="#ff9500">
+        <div className="notification-banner">
+          Hi {data?.Mentee?.name}, you have 1 upcoming & 3 pending meetings!
+        </div>
+      </BackgroundStyle>
+    );
+  };
+  return (
+    <AdminStyle>
+      <ProfileDashboard menteeInfo={data?.Mentee!} />
+      <div className="profile-container">
+        {renderNotificationBanner()}
+        <div className="info-cal-container">
+          <PersonalInfo menteeInfo={data?.Mentee!} />
+          <MeetingsCalendar />
+        </div>
+        <UpcomingMeetings />
+        <PendingMeetings />
+        <PastConnections />
+      </div>
+    </AdminStyle>
+  );
 };
 
 export default MenteeAdmin;
-
-const MenteeAdminStyle = styled.div`
-  display: flex;
-`;
